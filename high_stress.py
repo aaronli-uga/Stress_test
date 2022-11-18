9d3357c263b91c0e6bf20cd4799e38908ed0bb5d
aaronli-uga <ql61608@uga.edu>
2022-11-18 16:43:28 -0500

    update

--- high_stress.py
+++ high_stress.py
@@ -2,13 +2,14 @@
 Author: Qi7
 Date: 2022-11-18 14:48:17
 LastEditors: aaronli-uga ql61608@uga.edu
-LastEditTime: 2022-11-18 15:32:19
+LastEditTime: 2022-11-18 16:43:21
 Description: 
 '''
 import time
 from random import *
 import threading as th
 from pytimedinput import timedInput
+from playsound import playsound
 
 # define the countdown func.
 def shutdown():
@@ -24,6 +25,7 @@ score = 0
 t = 10 # 10 seconds per question
 num_questions = 30 # 5 mins test. 10 questions per mintus
 test_time = 300
+sound_file = "sounds/annoying.mp3"
 
 cmd = input("Enter s to start the test, or q to exit:\n")
 if cmd == "q":
@@ -40,10 +42,12 @@ while num_questions:
     userText, timedOut = timedInput(f"{a} X {b} = ?\n", timeout=t)
     if(timedOut):
         print("Timeout!! score reset!")
+        playsound(sound_file, block=False)
         score = 0
     elif (int(userText) == a*b):
         print("correct!")
         score += 1
     else:
         print("Wrong answer! Score reset!")
+        playsound(sound_file, block=False)
         score = 0
--- low_stress.py
+++ low_stress.py
@@ -2,13 +2,15 @@
 Author: Qi7
 Date: 2022-11-18 14:48:17
 LastEditors: aaronli-uga ql61608@uga.edu
-LastEditTime: 2022-11-18 15:32:36
+LastEditTime: 2022-11-18 16:42:27
 Description: 
 '''
 import time
 from random import *
 import threading as th
 from pytimedinput import timedInput
+from playsound import playsound
+
 
 # define the countdown func.
 def shutdown():
@@ -24,6 +26,7 @@ score = 0
 t = 10 # 10 seconds per question
 num_questions = 30 # 5 mins test. 10 questions per mintus
 test_time = 300
+sound_file = "sounds/annoying.mp3"
 
 cmd = input("Enter s to start the test, or q to exit:\n")
 if cmd == "q":
@@ -40,10 +43,12 @@ while num_questions:
     userText, timedOut = timedInput(f"{a} + {b} = ?\n", timeout=t)
     if(timedOut):
         print("Timeout!! score reset!")
+        playsound(sound_file, block=False)
         score = 0
     elif (int(userText) == a+b):
         print("correct!")
         score += 1
     else:
         print("Wrong answer! Score reset!")
+        playsound(sound_file, block=False)
         score = 0
